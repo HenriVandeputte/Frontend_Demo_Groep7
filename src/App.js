d3b06c30c461d30382fb672c7c3e205cb0f7781f
Henri Vandeputte <59021743+HenriVandeputte@users.noreply.github.com>
2022-02-16 11:27:17 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: "http://localhost:8080/api/v1/student/"
+})
+
+
+
+
+class App extends Component{
+
+  state = {
+
+    personen: []
+  }
+
+  constructor() {
+    super();
+    this.setState({personen : null})
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <button onClick={() => {api.get('/').then( res => {
+                console.log(res.data)
+                this.setState({personen : res.data})
+              })
+            }}> Klik hier </button>
+            <h2>
+              {this.state.personen.map(persoon => <h3 key={persoon.id}> {persoon.id} {persoon.name} {persoon.email } </h3>)}
+            </h2>
+          </header>
+        </div>
+    );
+  }
+
+
 }
 
 export default App;
